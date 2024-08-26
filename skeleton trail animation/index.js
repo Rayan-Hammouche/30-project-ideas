0e21facb357e260b962aa57917f96827d42dd33f
Yanis <166356311+Rayan-Hammouche@users.noreply.github.com>
2024-08-26 17:17:50 +0100

    Add files via upload

---
+++ skeleton trail animation/index.js
@@ -0,0 +1,18 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event) => {
+    const xPos = event.offsetX;
+    const yPos = event.offsetY;
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "100px";
+    spanEl.style.height = size + "100px";
+    const color = Math.random();
+    spanEl.style.color = color;
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
+});
